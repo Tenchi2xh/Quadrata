2cced516fcc656c09ea3a3fec4f5a31d1b22c8d1
Hamza Haiken <hamza.haiken@ordina.nl>
2017-10-29 19:00:34 +0100

    Add letter u

--- quadrata/hands/foundational.py
+++ quadrata/hands/foundational.py
@@ -16,16 +16,20 @@ club = Stroke(l(cl, cl))
 
 tail = q(5, 10, 20, 5)
 
-short_stem_without_club = Stroke(v(xh), tail)
-descender_stem_without_club = Stroke(v(xh + de), tail)
-
 n_curve = Stroke(c(sw, -cl, 2*sw, -cl, 2*sw, cl), v(sh), tail)
+u_curve = Stroke(c(0, cl, 1.5*sw, cl, 2.5*sw, 0))
 o_lower_curve = Stroke(c(-nw, nw, 0, sh + cy + nw, 3*sw, sh))
 q_upper_curve = Stroke(c(1.5*nw, -nw, sw*4 - nw, -cm, sw*4, -cm))
-u_lower_curve = Stroke()
+
+short_stem_without_club = Stroke(v(xh), tail)
+short_stem_without_club_long_tail = Stroke(v(xh), u_curve)
+descender_stem_without_club = Stroke(v(xh + de), tail)
 
 short_stem = (Letter()
     .add(short_stem_without_club)
+    .add(club, (-cl - 0.5, cy)))
+short_stem_long_tail = (Letter()
+    .add(short_stem_without_club_long_tail)
     .add(club, (-cl - 0.5, cy)))
 letter_n = short_stem.add_relative(n_curve, (cl + sw, 0))
 letter_m = letter_n.add_relative(n_curve, (3 * sw, 0))
@@ -33,6 +37,7 @@ letter_q = (Letter()
     .add(o_lower_curve, (0, cm))
     .add_relative(q_upper_curve, (0, 0))
     .add(descender_stem_without_club, (sw * 4, 0)))
+letter_u = short_stem_long_tail.add(short_stem, (3.5 * sw, 0))
 
 foundational = Hand(name="Foundational Hand",
                     nib=nib,
@@ -42,6 +47,8 @@ foundational = Hand(name="Foundational Hand",
                     em=sw * 6)
 
 foundational.glyphs = {
+    "m": letter_m,
     "n": letter_n,
-    "m": letter_m
+    "q": letter_q,
+    "u": letter_u
 }
