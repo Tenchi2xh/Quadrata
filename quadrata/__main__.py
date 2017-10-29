3e0f83ebfb8ebc47bef60bd17c88c68d005d5d41
Hamza Haiken <hamza.haiken@ordina.nl>
2017-10-29 14:21:13 +0100

    Remove debug print

--- quadrata/__main__.py
+++ quadrata/__main__.py
@@ -11,9 +11,7 @@ dwg = svgwrite.Drawing()
 
 group = foundational.letter_m.form(dwg, nib)
 group["transform"] = "translate(50, 50)"
-print(group.tostring())
 
 dwg.add(group)
-
 xml = dwg.tostring()
 util.preview(xml)
